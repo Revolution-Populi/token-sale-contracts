0ff0b5bca6070cac5449fc62710e33d298cc9504
Dmitry Abrosimov <d.abrosimov@axioma.lv>
2020-01-28 01:27:48 +0100

    Updated DSToken and DSTokenBase

--- contracts/DSToken.sol
+++ contracts/DSToken.sol
@@ -8,48 +8,77 @@ contract DSToken is DSTokenBase(0), DSStop {
     bytes32  public  symbol;
     uint256  public  decimals = 18; // standard token precision. override to customize
 
-    function DSToken(bytes32 symbol_) {
+    constructor(bytes32 symbol_) public {
         symbol = symbol_;
     }
 
-    function transfer(address dst, uint wad) stoppable note returns (bool) {
-        return super.transfer(dst, wad);
+    event Mint(address indexed guy, uint wad);
+    event Burn(address indexed guy, uint wad);
+
+    function approve(address guy) public stoppable returns (bool) {
+        return super.approve(guy, uint(-1));
     }
 
-    function transferFrom(
-        address src, address dst, uint wad
-    ) stoppable note returns (bool) {
-        return super.transferFrom(src, dst, wad);
-    }
-
-    function approve(address guy, uint wad) stoppable note returns (bool) {
+    function approve(address guy, uint wad) public stoppable returns (bool) {
         return super.approve(guy, wad);
     }
 
-    function push(address dst, uint128 wad) returns (bool) {
-        return transfer(dst, wad);
+    function transferFrom(address src, address dst, uint wad)
+    public
+    stoppable
+    returns (bool)
+    {
+        if (src != msg.sender && _approvals[src][msg.sender] != uint(-1)) {
+            require(_approvals[src][msg.sender] >= wad, "ds-token-insufficient-approval");
+            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        }
+
+        require(_balances[src] >= wad, "ds-token-insufficient-balance");
+        _balances[src] = sub(_balances[src], wad);
+        _balances[dst] = add(_balances[dst], wad);
+
+        emit Transfer(src, dst, wad);
+
+        return true;
     }
 
-    function pull(address src, uint128 wad) returns (bool) {
-        return transferFrom(src, msg.sender, wad);
+    function push(address dst, uint wad) public {
+        transferFrom(msg.sender, dst, wad);
+    }
+    function pull(address src, uint wad) public {
+        transferFrom(src, msg.sender, wad);
+    }
+    function move(address src, address dst, uint wad) public {
+        transferFrom(src, dst, wad);
     }
 
-    function mint(uint128 wad) auth stoppable note {
-        _balances[msg.sender] = add(_balances[msg.sender], wad);
+    function mint(uint wad) public {
+        mint(msg.sender, wad);
+    }
+    function burn(uint wad) public {
+        burn(msg.sender, wad);
+    }
+    function mint(address guy, uint wad) public auth stoppable {
+        _balances[guy] = add(_balances[guy], wad);
         _supply = add(_supply, wad);
+        emit Mint(guy, wad);
     }
+    function burn(address guy, uint wad) public auth stoppable {
+        if (guy != msg.sender && _approvals[guy][msg.sender] != uint(-1)) {
+            require(_approvals[guy][msg.sender] >= wad, "ds-token-insufficient-approval");
+            _approvals[guy][msg.sender] = sub(_approvals[guy][msg.sender], wad);
+        }
 
-    function burn(uint128 wad) auth stoppable note {
-        _balances[msg.sender] = sub(_balances[msg.sender], wad);
+        require(_balances[guy] >= wad, "ds-token-insufficient-balance");
+        _balances[guy] = sub(_balances[guy], wad);
         _supply = sub(_supply, wad);
+        emit Burn(guy, wad);
     }
 
     // Optional token name
-
     bytes32   public  name = "";
 
-    function setName(bytes32 name_) auth {
+    function setName(bytes32 name_) public auth {
         name = name_;
     }
-
 }
--- contracts/DSTokenBase.sol
+++ contracts/DSTokenBase.sol
@@ -8,51 +8,48 @@ contract DSTokenBase is ERC20, DSMath {
     mapping (address => uint256)                       _balances;
     mapping (address => mapping (address => uint256))  _approvals;
 
-    function DSTokenBase(uint256 supply) {
+    constructor(uint supply) public {
         _balances[msg.sender] = supply;
         _supply = supply;
     }
 
-    function totalSupply() view returns (uint256) {
+    function totalSupply() public view returns (uint) {
         return _supply;
     }
-    function balanceOf(address src) view returns (uint256) {
+    function balanceOf(address src) public view returns (uint) {
         return _balances[src];
     }
-    function allowance(address src, address guy) view returns (uint256) {
+    function allowance(address src, address guy) public view returns (uint) {
         return _approvals[src][guy];
     }
 
-    function transfer(address dst, uint wad) returns (bool) {
-        assert(_balances[msg.sender] >= wad);
-
-        _balances[msg.sender] = sub(_balances[msg.sender], wad);
-        _balances[dst] = add(_balances[dst], wad);
-
-        Transfer(msg.sender, dst, wad);
-
-        return true;
+    function transfer(address dst, uint wad) public returns (bool) {
+        return transferFrom(msg.sender, dst, wad);
     }
 
-    function transferFrom(address src, address dst, uint wad) returns (bool) {
-        assert(_balances[src] >= wad);
-        assert(_approvals[src][msg.sender] >= wad);
+    function transferFrom(address src, address dst, uint wad)
+    public
+    returns (bool)
+    {
+        if (src != msg.sender) {
+            require(_approvals[src][msg.sender] >= wad, "ds-token-insufficient-approval");
+            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        }
 
-        _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        require(_balances[src] >= wad, "ds-token-insufficient-balance");
         _balances[src] = sub(_balances[src], wad);
         _balances[dst] = add(_balances[dst], wad);
 
-        Transfer(src, dst, wad);
+        emit Transfer(src, dst, wad);
 
         return true;
     }
 
-    function approve(address guy, uint256 wad) returns (bool) {
+    function approve(address guy, uint wad) public returns (bool) {
         _approvals[msg.sender][guy] = wad;
 
-        Approval(msg.sender, guy, wad);
+        emit Approval(msg.sender, guy, wad);
 
         return true;
     }
-
 }
